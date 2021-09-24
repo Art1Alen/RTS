5a194d11fa427a9c68e00abc051483e729172ea1
Артемий Собакин <72554995+Art1Alen@users.noreply.github.com>
2021-09-24 19:52:25 +0900

    RTS
    Outline

---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/BottomRightPresenter.cs
@@ -0,0 +1,49 @@
+﻿using Abstractions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserControlSystem
+{
+    public sealed class BottomRightPresenter : MonoBehaviour
+    {
+        [SerializeField] private Button _buttonAttake;
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Button _buttonMove;
+        [SerializeField] private TextMeshProUGUI _move;
+        [SerializeField] private Button _buttonPatrol;
+        [SerializeField] private TextMeshProUGUI _patrol;
+        [SerializeField] private Button _buttonProtectior;
+        [SerializeField] private TextMeshProUGUI _protectior;
+
+        [SerializeField] private SelectableValue _selectedValue;
+
+        private void Start()
+        {
+            _selectedValue.OnSelected += ONSelected;
+            ONSelected(_selectedValue.CurrentValue);
+        }
+
+        private void ONSelected(ISelectable selected)
+        {
+            _buttonAttake.enabled = selected != null;
+            _text.gameObject.SetActive(selected != null);
+            _text.enabled = selected != null;
+
+            _buttonMove.enabled = selected != null;
+            _move.gameObject.SetActive(selected != null);
+            _move.enabled = selected != null;
+
+            _buttonPatrol.enabled = selected != null;
+            _patrol.gameObject.SetActive(selected != null);
+            _patrol.enabled = selected != null;
+
+            _buttonProtectior.enabled = selected != null;
+            _protectior.gameObject.SetActive(selected != null);
+            _protectior.enabled = selected != null;
+
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Abstractions;
+using UnityEngine;
+using UserControlSystem;
+
+public sealed class MouseInteractionPresenter : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private SelectableValue _selectedObject;
+
+    private void Update()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        if(Physics.Raycast(ray, out raycastHit, 100))
+        {
+            var MouseInteractionPresenter = raycastHit.collider.GetComponent<MouseInteractionPresenter>();
+            if(MouseInteractionPresenter != null)
+            {
+                Debug.Log("OnHoverEnter");
+            }
+            else
+            {
+                Debug.Log("OnHoverExit");
+            }
+        }
+
+        if (!Input.GetMouseButtonUp(0))
+        {
+            return;
+        }
+        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+        if (hits.Length == 0)
+        {
+            return;
+        }
+        var selectable = hits
+            .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
+            .FirstOrDefault(c => c != null);
+        _selectedObject.SetValue(selectable);
+    }
+}
